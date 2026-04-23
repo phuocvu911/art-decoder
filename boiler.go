dddebe24818eb5bd1258206804f2915b7f39807d
Hoang Phuoc Vu <phuocvu091198@gmail.com>
2026-04-23 12:10:28 +0300

    printUsage()

--- boiler.go
+++ boiler.go
@@ -153,26 +153,6 @@ func encodeMultiLine(lines []string) []string {
 	return result
 }
 
-func printUsage() {
-	fmt.Fprintln(os.Stderr, `Usage:
-  art-decoder [flags] "<encoded_string>"
-  art-decoder --multi [flags]   (reads lines from stdin)
-
-Flags:
-  --encode, -e    Encode plain text into art-decoder notation
-  --multi,  -m    Read multiple lines from stdin
-  --paint,  -p    Colorize output: each unique character gets its own ANSI color
-  --help,   -h    Show this help
-
-Examples:
-  art-decoder "[5 #][5 -_]-[5 #]"
-  art-decoder --encode "#####-_-_-_-_-_-#####"
-  art-decoder --multi < file.encoded
-  art-decoder --encode --multi < file.art
-  art-decoder --paint "[5 #][5 -_]-[5 #]"
-  art-decoder --paint --multi < plane.encoded`)
-}
-
 func main() {
 	args := os.Args[1:]
 
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+func printUsage() {
+	fmt.Fprintln(os.Stderr, `Usage:
+  art-decoder [flags] "<encoded_string>"
+  art-decoder --multi [flags]   (reads lines from stdin)
+
+Flags:
+  --encode, -e    Encode plain text into art-decoder notation
+  --multi,  -m    Read multiple lines from stdin
+  --paint,  -p    Colorize output: each unique character gets its own ANSI color
+  --help,   -h    Show this help
+
+Examples:
+  art-decoder "[5 #][5 -_]-[5 #]"
+  art-decoder --encode "#####-_-_-_-_-_-#####"
+  art-decoder --multi < file.encoded
+  art-decoder --encode --multi < file.art
+  art-decoder --paint "[5 #][5 -_]-[5 #]"
+  art-decoder --paint --multi < file.encoded`)
+}
 
 func main() {
-	input := os.Args[1]
+	input := os.Args[1:]
+
+	if len(input) == 0 {
+		printUsage()
+		os.Exit(1)
+	}
 }
