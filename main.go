6316e7d4aa2dc0edd48e8f296869dc4f8f3ed7c3
Hoang Phuoc Vu <iamphuocvux@gmail.com>
2026-04-27 21:13:59 +0300

    fix: clarify paint mode flag usage in README and improve input handling in main function

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"os"
 
 	u "art-decoder/utils"
@@ -13,5 +15,25 @@ func main() {
 	if helpMode {
 		u.PrintUsage()
 		os.Exit(0)
+	}
+	args := flag.Args()
+
+	if multiMode {
+		lines := make([]string, 0)
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		for _, line := range lines {
+			fmt.Println(u.Decode(line))
+		}
+	} else {
+		for _, arg := range args {
+			fmt.Println(u.Decode(arg))
+		}
 	}
 }
